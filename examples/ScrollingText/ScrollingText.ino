2ff6739aa80eae61ed146c323cecd60a4a947ca6
Patrick Neggie <patar9958@users.noreply.github.com>
2018-05-23 14:52:13 -0400

    added ScrollingText.ino to examples

---
+++ examples/ScrollingText/ScrollingText.ino
@@ -0,0 +1,62 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+int pinCS = 10; // Attach CS to this pin, DIN to MOSI and CLK to SCK (cf http://arduino.cc/en/Reference/SPI )
+// i.e. connect CS to 10, DIN to 11, CLK to 13
+int numberOfHorizontalDisplays = 8;
+int numberOfVerticalDisplays = 1;
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+String tape = "TinkerTech :)";
+int wait = 20; // In milliseconds
+
+int spacer = 1;
+int width = 5 + spacer; // The font width is 5 pixels
+
+void setup() {
+
+  matrix.setIntensity(7); // Use a value between 0 and 15 for brightness
+
+  matrix.setPosition(0, 0, 0); // The first display is at <0, 0>
+  matrix.setPosition(1, 1, 0); // The second display is at <1, 0>
+  matrix.setPosition(2, 2, 0); // The third display is at <2, 0>
+  matrix.setPosition(3, 3, 0); // And the last display is at <3, 0>
+
+  // TODO: IS THERE ANY WAY TO WRITE THIS IN A LOOP? (ask campers)
+  matrix.setRotation(0, 1);
+  matrix.setRotation(1, 1);
+  matrix.setRotation(2, 1);
+  matrix.setRotation(3, 1);
+  matrix.setRotation(4, 1);
+  matrix.setRotation(5, 1);
+  matrix.setRotation(6, 1);
+  matrix.setRotation(7, 1);
+}
+
+void loop() {
+
+  for ( int i = 0 ; i < width * tape.length() + matrix.width() - 1 - spacer; i++ ) {
+
+    matrix.fillScreen(LOW);
+
+    int letter = i / width;
+    int x = (matrix.width() - 1) - i % width;
+    int y = (matrix.height() - 8) / 2; // center the text vertically
+
+    while ( x + width - spacer >= 0 && letter >= 0 ) {
+      if ( letter < tape.length() ) {
+        matrix.drawChar(x, y, tape[letter], HIGH, LOW, 1);
+      }
+
+      letter--;
+      x -= width;
+    }
+
+    matrix.write(); // Send bitmap to display
+
+    delay(wait);
+  }
+}
+
